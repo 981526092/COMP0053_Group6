af32d0b6effdd9796ac29cc5b71986b0f9dc42dd
chloe1999 <yyinai705@gmail.com>
2023-03-21 23:48:04 +0000

    add hybrid fusion angle_energy model and pipeline

--- Software/hybrid_model.py
+++ Software/hybrid_model.py
@@ -386,3 +386,237 @@ def build_bi_cnn_banet_model():
     # model.summary()
 
     return model
+
+
+def build_bi_cnn_banet_angle_model(first_cnn=64, second_cnn=32, lstm_units=32):
+    '''
+    Hyper-params list:
+    Epoch = 20
+    Optimizer = Adam
+    Initial learning rate = 0.0005
+    batch_size = 500
+    DO NOT CHANGE THIS FUNCTION
+    '''
+    timestep = 180   # length of an input frame
+    dimension = 30   # dimension of an input frame, 30 = 13 joint angles + 13 joint energies + 4 sEMG
+    BodyNum = 13     # number of body segments (different sensors) to consider
+
+    #Model 1: Temporal Information encoding model (keras Model API)
+    # For angles
+    singleinput = Input(shape=(180, 2,))
+    lstm_units = lstm_units
+
+    Conv1D1 = Conv1D(first_cnn, first_cnn, strides = 1)(singleinput)
+    Batch1 = BatchNormalization()(Conv1D1)
+    Relu1 = Activation('relu')(Batch1)
+    Dropout1 = Dropout(0.5)(Relu1)
+
+    Conv1D2 = Conv1D(second_cnn, second_cnn, strides = 1)(Dropout1)
+    Batch2 = BatchNormalization()(Conv1D2)
+    Relu2 = Activation('relu')(Batch2)
+    Dropout2 = Dropout(0.5)(Relu2)
+
+    TemporalProcessmodel = Model(inputs=[singleinput], outputs=[Dropout2])
+
+    # for Semg
+    singleinput_Semg = Input(shape=(180, 2,))
+
+    Conv1D1_Semg = Conv1D(first_cnn, first_cnn, strides = 1)(singleinput_Semg)
+    Batch1_Semg = BatchNormalization()(Conv1D1_Semg)
+    Relu1_Semg = Activation('relu')(Batch1_Semg)
+    Dropout1_Semg = Dropout(0.5)(Relu1_Semg)
+
+    Conv1D2_Semg = Conv1D(second_cnn, second_cnn, strides = 1)(Dropout1_Semg)
+    Batch2_Semg = BatchNormalization()(Conv1D2_Semg)
+    Relu2_Semg = Activation('tanh')(Batch2_Semg)
+    Dropout2_Semg = Dropout(0.5)(Relu2_Semg)
+
+    TemporalProcessmodel_Semg = Model(inputs=[singleinput_Semg], outputs=[Dropout2_Semg])
+
+    # Model 2: Main Structure, starting with independent temporal information encoding and attention learning
+    inputs = Input(shape=(180, 30,))        # The input data is 180 timesteps by 30 features (13 angles + 13 energies + 4 sEMG)
+                                            # The information each body segment included is the angle and energy
+    
+    Angle1 = crop(2, 0, 1)(inputs)
+    Acc1 = crop(2, 13, 14)(inputs)
+    B1 = concatenate([Angle1, Acc1], axis=-1)
+    Anglefullout1 = TemporalProcessmodel(B1)
+    TemporalAttention1 = Conv1D(1, 1, strides=1)(Anglefullout1) # Temporal Attention Module for each body segment will starts with 1 X 1 Convolution
+    TemporalAttention1 = Softmax(axis=-2, name='TemporalAtten1')(TemporalAttention1) # You need Keras >= 2.1.3 to call Softmax as a layer
+    AngleAttout1 = multiply([Anglefullout1, TemporalAttention1])
+    AngleAttout1 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout1)
+    Blast1 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout1)
+
+    Angle2 = crop(2, 1, 2)(inputs)
+    Acc2 = crop(2, 14, 15)(inputs)
+    B2 = concatenate([Angle2, Acc2], axis=-1)
+    Anglefullout2 = TemporalProcessmodel(B2)
+    TemporalAttention2 = Conv1D(1, 1, strides=1)(Anglefullout2)
+    TemporalAttention2 = Softmax(axis=-2, name='TemporalAtten2')(TemporalAttention2)
+    AngleAttout2 = multiply([Anglefullout2, TemporalAttention2])
+    AngleAttout2 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout2)
+    Blast2 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout2)
+
+    Angle3 = crop(2, 2, 3)(inputs)
+    Acc3 = crop(2, 15, 16)(inputs)
+    B3 = concatenate([Angle3, Acc3], axis=-1)
+    Anglefullout3 = TemporalProcessmodel(B3)
+    TemporalAttention3 = Conv1D(1, 1, strides=1)(Anglefullout3)
+    TemporalAttention3 = Softmax(axis=-2, name='TemporalAtten3')(TemporalAttention3)
+    AngleAttout3 = multiply([Anglefullout3, TemporalAttention3])
+    AngleAttout3 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout3)
+    Blast3 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout3)
+
+    Angle4 = crop(2, 3, 4)(inputs)
+    Acc4 = crop(2, 16, 17)(inputs)
+    B4 = concatenate([Angle4, Acc4], axis=-1)
+    Anglefullout4 = TemporalProcessmodel(B4)
+    TemporalAttention4 = Conv1D(1, 1, strides=1)(Anglefullout4)
+    TemporalAttention4 = Softmax(axis=-2, name='TemporalAtten4')(TemporalAttention4)
+    AngleAttout4 = multiply([Anglefullout4, TemporalAttention4])
+    AngleAttout4 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout4)
+    Blast4 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout4)
+
+    Angle5 = crop(2, 4, 5)(inputs)
+    Acc5 = crop(2, 17, 18)(inputs)
+    B5 = concatenate([Angle5, Acc5], axis=-1)
+    Anglefullout5 = TemporalProcessmodel(B5)
+    TemporalAttention5 = Conv1D(1, 1, strides=1)(Anglefullout5)
+    TemporalAttention5 = Softmax(axis=-2, name='TemporalAtten5')(TemporalAttention5)
+    AngleAttout5 = multiply([Anglefullout5, TemporalAttention5])
+    AngleAttout5 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout5)
+    Blast5 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout5)
+
+    Angle6 = crop(2, 5, 6)(inputs)
+    Acc6 = crop(2, 18, 19)(inputs)
+    B6 = concatenate([Angle6, Acc6], axis=-1)
+    Anglefullout6 = TemporalProcessmodel(B6)
+    TemporalAttention6 = Conv1D(1, 1, strides=1)(Anglefullout6)
+    TemporalAttention6 = Softmax(axis=-2, name='TemporalAtten6')(TemporalAttention6)
+    AngleAttout6 = multiply([Anglefullout6, TemporalAttention6])
+    AngleAttout6 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout6)
+    Blast6 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout6)
+
+    Angle7 = crop(2, 6, 7)(inputs)
+    Acc7 = crop(2, 19, 20)(inputs)
+    B7 = concatenate([Angle7, Acc7], axis=-1)
+    Anglefullout7 = TemporalProcessmodel(B7)
+    TemporalAttention7 = Conv1D(1, 1, strides=1)(Anglefullout7)
+    TemporalAttention7 = Softmax(axis=-2, name='TemporalAtten7')(TemporalAttention7)
+    AngleAttout7 = multiply([Anglefullout7, TemporalAttention7])
+    AngleAttout7 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout7)
+    Blast7 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout7)
+
+    Angle8 = crop(2, 7, 8)(inputs)
+    Acc8 = crop(2, 20, 21)(inputs)
+    B8 = concatenate([Angle8, Acc8], axis=-1)
+    Anglefullout8 = TemporalProcessmodel(B8)
+    TemporalAttention8 = Conv1D(1, 1, strides=1)(Anglefullout8)
+    TemporalAttention8 = Softmax(axis=-2, name='TemporalAtten8')(TemporalAttention8)
+    AngleAttout8 = multiply([Anglefullout8, TemporalAttention8])
+    AngleAttout8 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout8)
+    Blast8 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout8)
+
+    Angle9 = crop(2, 8, 9)(inputs)
+    Acc9 = crop(2, 21, 22)(inputs)
+    B9 = concatenate([Angle9, Acc9], axis=-1)
+    Anglefullout9 = TemporalProcessmodel(B9)
+    TemporalAttention9 = Conv1D(1, 1, strides=1)(Anglefullout9)
+    TemporalAttention9 = Softmax(axis=-2, name='TemporalAtten9')(TemporalAttention9)
+    AngleAttout9 = multiply([Anglefullout9, TemporalAttention9])
+    AngleAttout9 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout9)
+    Blast9 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout9)
+
+    Angle10 = crop(2, 9, 10)(inputs)
+    Acc10 = crop(2, 22, 23)(inputs)
+    B10 = concatenate([Angle10, Acc10], axis=-1)
+    Anglefullout10 = TemporalProcessmodel(B10)
+    TemporalAttention10 = Conv1D(1, 1, strides=1)(Anglefullout10)
+    TemporalAttention10 = Softmax(axis=-2, name='TemporalAtten10')(TemporalAttention10)
+    AngleAttout10 = multiply([Anglefullout10, TemporalAttention10])
+    AngleAttout10 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout10)
+    Blast10 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout10)
+
+    Angle11 = crop(2, 10, 11)(inputs)
+    Acc11 = crop(2, 23, 24)(inputs)
+    B11 = concatenate([Angle11, Acc11], axis=-1)
+    Anglefullout11 = TemporalProcessmodel(B11)
+    TemporalAttention11 = Conv1D(1, 1, strides=1)(Anglefullout11)
+    TemporalAttention11 = Softmax(axis=-2, name='TemporalAtten11')(TemporalAttention11)
+    AngleAttout11 = multiply([Anglefullout11, TemporalAttention11])
+    AngleAttout11 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout11)
+    Blast11 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout11)
+
+    Angle12 = crop(2, 11, 12)(inputs)
+    Acc12 = crop(2, 24, 25)(inputs)
+    B12 = concatenate([Angle12, Acc12], axis=-1)
+    Anglefullout12 = TemporalProcessmodel(B12)
+    TemporalAttention12 = Conv1D(1, 1, strides=1)(Anglefullout12)
+    TemporalAttention12 = Softmax(axis=-2, name='TemporalAtten12')(TemporalAttention12)
+    AngleAttout12 = multiply([Anglefullout12, TemporalAttention12])
+    AngleAttout12 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout12)
+    Blast12 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout12)
+
+    Angle13 = crop(2, 12, 13)(inputs)
+    Acc13 = crop(2, 25, 26)(inputs)
+    B13 = concatenate([Angle13, Acc13], axis=-1)
+    Anglefullout13 = TemporalProcessmodel(B13)
+    TemporalAttention13 = Conv1D(1, 1, strides=1)(Anglefullout13)
+    TemporalAttention13 = Softmax(axis=-2, name='TemporalAtten13')(TemporalAttention13)
+    AngleAttout13 = multiply([Anglefullout13, TemporalAttention13])
+    AngleAttout13 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(AngleAttout13)
+    Blast13 = Permute((2, 1), input_shape=(1, lstm_units))(AngleAttout13)
+
+    # Semg
+    SemgBodyNum = 2     # number of body segments (different sensors) to consider
+    # lower back
+    Semg1 = crop(2, 26, 27)(inputs)
+    Semg2 = crop(2, 27, 28)(inputs)
+    B14 = concatenate([Semg1, Semg2], axis=-1)
+    Semgfullout1 = TemporalProcessmodel_Semg(B14)
+    TemporalAttention14 = Conv1D(1, 1, strides=1)(Semgfullout1)
+    TemporalAttention14 = Softmax(axis=-2, name='TemporalAtten14')(TemporalAttention14)
+    SemgAttout1 = multiply([Semgfullout1, TemporalAttention14])
+    SemgAttout1 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(SemgAttout1)
+    Blast14 = Permute((2, 1), input_shape=(1, lstm_units))(SemgAttout1)
+
+    # upper back
+    Semg3 = crop(2, 28, 29)(inputs)
+    Semg4 = crop(2, 29, 30)(inputs)
+    B15 = concatenate([Semg3, Semg4], axis=-1)
+    Semgfullout2 = TemporalProcessmodel_Semg(B15)
+    TemporalAttention15 = Conv1D(1, 1, strides=1)(Semgfullout2)
+    TemporalAttention15 = Softmax(axis=-2, name='TemporalAtten15')(TemporalAttention15)
+    SemgAttout2 = multiply([Semgfullout2, TemporalAttention15])
+    SemgAttout2 = Lambda(lambda x: sum(x, axis=1, keepdims=True))(SemgAttout2)
+    Blast15 = Permute((2, 1), input_shape=(1, lstm_units))(SemgAttout2)
+
+    # Model 3: Feature Concatenation for Bodily Attention Learning
+
+    DATA = concatenate([Blast1, Blast2, Blast3, Blast4, Blast5, Blast6, Blast7, Blast8,
+                        Blast9, Blast10, Blast11, Blast12, Blast13
+                        ], axis=2)
+
+    a = Dense(BodyNum, activation='tanh')(DATA)
+    a = Dense(BodyNum, activation='softmax', name='bodyattention')(a)
+    attentionresult = multiply([DATA, a])
+    attentionresult = Flatten()(attentionresult)
+    output1 = Dense(2, activation='softmax')(attentionresult)
+
+   
+    
+    # Semg
+
+    DATA_Semg = concatenate([Blast14, Blast15], axis=2)
+
+    # Bodily Attention Module
+    a2 = Dense(SemgBodyNum, activation='tanh')(DATA_Semg)
+    a2 = Dense(SemgBodyNum, activation='softmax', name='bodyattention2')(a2)
+    attentionresult2 = multiply([DATA_Semg, a2])
+    attentionresult2 = Flatten()(attentionresult2)
+    output2 = Dense(2, activation='softmax')(attentionresult2)
+
+    # late fusion
+    output = output1*0.4 + output2*0.6
+    model = Model(inputs=inputs, outputs=output)
+    return model
