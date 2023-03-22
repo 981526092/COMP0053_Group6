a3c458ec920b881aa4b9fd6c37d138b1d2ab0884
Zekun Wu <wu981526092@163.com>
2023-03-22 15:09:10 +0000

    add CV method

--- Software/evaluation_utils.py
+++ Software/evaluation_utils.py
@@ -1,52 +1,55 @@
 import numpy as np
 from sklearn.metrics import f1_score
-from sklearn.model_selection import LeavePOut, GroupKFold, LeaveOneOut, StratifiedKFold, TimeSeriesSplit
+from sklearn.model_selection import LeavePOut, StratifiedKFold, TimeSeriesSplit
 from keras.models import clone_model
 from model_utils import model_pipeline
 from data_utils import load_data
+from sklearn.metrics import f1_score
 
 # Perform cross-validation and return the scores and best model
-def cross_validation(model,train_participant_num,valid_participant_num,cv_selection= 'LeavePOut',p = 15,epoch = 50):
-
+def LeavePOut_CV(model,train_participant_num,p = 15,epoch = 50):
     # Choose cross-validation method based on the input parameter
-    if cv_selection == 'LeavePOut':
-        cv_method = LeavePOut(p=p)
-    elif cv_selection == 'LeaveOneOut':
-        cv_method = LeaveOneOut()
-    else:
-        raise ValueError("Invalid cross-validation selection. Choose from 'LeavePOut', 'LeaveOneOut'.")
-
+    cv_method = LeavePOut(p=p)
     scores = []
     best_f1 = 0
     best_model = None
-    X_validation, y_validation = load_data(valid_participant_num, data_set="validation")
-
     # Iterate through the cross-validation splits
     for train, valid in cv_method.split(train_participant_num):
         train_participants = [train_participant_num[i] for i in train]
         valid_participants = [train_participant_num[i] for i in valid]
-
         x_train,y_train = load_data(train_participants,data_set="train")
         x_valid,y_valid = load_data(valid_participants,data_set="train")
-
-        # Concatenate the validation data to the training data
-        x_valid = np.concatenate((X_validation,x_valid),axis=0)
-        y_valid = np.concatenate((y_validation,y_valid),axis=0)
-
         # Use the model_pipeline function to train and evaluate the model
         y_pred, y_true, H = model_pipeline(model, x_train, y_train, x_valid, y_valid, save_model=False, print_results=False,epoch=epoch)
-
         score = f1_score(y_true, y_pred, average='macro')
-
         # Store the score
         scores.append(score)
-
         # Check if this is the best model so far
         if score > best_f1:
             best_f1 = score
             best_model = clone_model(model)
             best_model.set_weights(model.get_weights())
+    return np.array(scores), best_model
 
+def TimeSeriesSplit_CV(model, X_train, y_train, n_splits=5, epoch=50):
+    cv_method = TimeSeriesSplit(n_splits=n_splits)
+    scores = []
+    best_f1 = 0
+    best_model = None
+    # Iterate through the cross-validation splits
+    for train_idx, valid_idx in cv_method.split(X_train):
+        x_train, x_valid = X_train[train_idx], X_train[valid_idx]
+        y_train_fold, y_valid_fold = y_train[train_idx], y_train[valid_idx]
+        # Use the model_pipeline function to train and evaluate the model
+        y_pred, y_true, H = model_pipeline(model, x_train, y_train_fold, x_valid, y_valid_fold, save_model=False, print_results=False, epoch=epoch)
+        score = f1_score(y_true, y_pred, average='macro')
+        # Store the score
+        scores.append(score)
+        # Check if this is the best model so far
+        if score > best_f1:
+            best_f1 = score
+            best_model = clone_model(model)
+            best_model.set_weights(model.get_weights())
     return np.array(scores), best_model
 
 # Compute the confusion matrix for binary classification
